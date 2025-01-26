083eed33e15f9a647f7315237747ab8d0dd4be2d
Lijian <ilovekechunrui@gmail.com>
2025-01-26 22:31:28 +0800

    upload all the files

---
+++ chapter4_1/src/chapter4_1/Student.java
@@ -0,0 +1,12 @@
+package chapter4_1;
+
+class Student extends Person{
+		private String school;
+		public void setSchool(String school) {
+			this.school = school;
+		}
+		public String getSchool() {
+			return this.school;
+		}
+
+}
