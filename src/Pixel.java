dd6012aae735942d2992f83cae6b8db3d83134d9
Lijian <ilovekechunrui@gmail.com>
2025-01-26 17:52:45 +0800

    first commit

---
+++ src/Pixel.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+
+class Point{
+	private int x, y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Point() {this(0,0);}
+	
+	public String toString() {
+		return "点："+x+","+y;
+	}
+	
+}
+public class Pixel extends Point {
+	Color c;
+	
+	public Pixel(int x,int y, Color c) {
+		super(x,y);
+		this.c = c;
+	}
+	
+	public String toString() {
+		return super.toString()+"颜色："+c;
+	}
+	
+	public static void main(String[] a) {
+		Pixel x = new Pixel(3, 24, Color.blue);
+		System.out.println(x);
+	}
+
+}
---
+++ src/first_try.java
@@ -0,0 +1,14 @@
+class Parent{
+	String my;
+	public Parent(String x) {
+		my = x;
+	}
+}
+/* public class subclass extends Parent{} 
+ * 编译报错，因为父类没有无参的默认构造方法
+ */
+public class first_try {
+		public static void main(String[] args) {
+			
+		}
+}
